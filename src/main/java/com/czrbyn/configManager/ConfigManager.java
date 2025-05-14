baed901eb115dcb900d8ac0ee58d6242cf6f7ca3
czrbynnn <corbynjearymain@gmail.com>
2025-05-14 12:54:53 +0100

    Added tab completion.

--- src/main/java/com/czrbyn/configManager/ConfigManager.java
+++ src/main/java/com/czrbyn/configManager/ConfigManager.java
@@ -1,17 +1,26 @@
 package com.czrbyn.configManager;
 
+import com.czrbyn.configManager.commands.ConfigManagerTabCompletor;
+import com.czrbyn.configManager.commands.MainCommand;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class ConfigManager extends JavaPlugin {
 
+    private MainCommand mcmd;
+    private ConfigManagerTabCompletor cmtc;
+
     @Override
     public void onEnable() {
-        // Plugin startup logic
+        mcmd = new MainCommand();
+        cmtc = new ConfigManagerTabCompletor();
+
+        getCommand("configmanager").setExecutor(mcmd);
+        getCommand("configmanager").setTabCompleter(cmtc);
 
     }
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
+
     }
 }
---
+++ src/main/java/com/czrbyn/configManager/commands/ConfigManagerTabCompletor.java
@@ -0,0 +1,40 @@
+package com.czrbyn.configManager.commands;
+
+import org.bukkit.Bukkit;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.command.TabCompleter;
+import org.bukkit.plugin.Plugin;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class ConfigManagerTabCompletor implements TabCompleter {
+    @Override
+    public List<String> onTabComplete(CommandSender sender, Command command, String label, String[] args) {
+        if (args.length == 1) {
+            return Arrays.asList("addList", "addString", "createList", "createString", "removeKey", "setValue");
+        }
+
+        if (args.length == 2) {
+            List<String> pluginNames = new ArrayList<>();
+            for (Plugin plugin : Bukkit.getPluginManager().getPlugins()) {
+                String name = plugin.getName();
+                if (name.toLowerCase().startsWith(args[1].toLowerCase())) {
+                    pluginNames.add(name);
+                }
+            }
+            return pluginNames;
+        }
+
+        if (args.length == 3) {
+            return Arrays.asList("config.yml", "settings.yml");
+        }
+
+        return Collections.emptyList();
+    }
+
+}
